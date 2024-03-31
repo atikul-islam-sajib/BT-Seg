92368fef71fa51e669f71f248a921d523351efff
Atikul Islam Sajib <asajib142099@bscse.uiu.ac.bd>
2024-03-31 22:40:27 +0200

    utils file is updated

--- src/utils.py
+++ src/utils.py
@@ -16,3 +16,21 @@ def load(filename):
         return joblib.load(filename)
     else:
         raise Exception("Filename cannot be None".capitalize())
+
+
+def device_init(device="mps"):
+    if device == "mps":
+        return torch.device("mps" if torch.backends.mps.is_available() else "cpu")
+    elif device == "cuda":
+        return torch.device("cuda" if torch.cuda.is_available() else "cpu")
+    else:
+        return torch.device("cpu")
+
+
+def weight_init(m):
+    classname = m.__class__.__name__
+    if classname.find("Conv") != -1:
+        nn.init.kaiming_normal_(m.weight, mode="fan_out", nonlinearity="relu")
+    elif classname.find("BatchNorm") != -1:
+        nn.init.normal_(m.weight, 1.0, 0.02)
+        nn.init.zeros_(m.bias)
