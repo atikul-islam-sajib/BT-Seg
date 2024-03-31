7e4bfad0495db7e29f599633c746e40dff5f4d13
Atikul Islam Sajib <asajib142099@bscse.uiu.ac.bd>
2024-03-31 23:02:45 +0200

    trainer code is updated with all parametets a

--- src/trainer.py
+++ src/trainer.py
@@ -1,4 +1,3 @@
-import sys
 import os
 import logging
 import argparse
@@ -9,59 +8,74 @@ import torch
 import torch.nn as nn
 from torchvision.utils import save_image
 
-sys.path.append("src/")
-
 from config import (
-    PROCESSED_PATH,
     TRAIN_CHECKPOINT_PATH,
     TEST_CHECKPOINT_PATH,
     TRAIN_IMAGES_PATH,
     METRICS_PATH,
 )
-from utils import load, dump, weight_init, define_device
-from UNet import UNet
+from utils import load, dump, device_init
+from helpers import helpers
 
 
 class Trainer:
     """
-    A class responsible for training a U-Net model for image segmentation tasks.
+    A trainer class for training a U-Net or Attention U-Net model for image segmentation tasks.
 
-    Attributes:
-        epochs (int): Number of epochs to train the model.
-        lr (float): Learning rate for the optimizer.
-        loss (str): Name of the loss function to be used. Currently supports 'dice' loss.
-        smooth_value (float): Smoothing value for the Dice loss function to avoid division by zero.
-        beta1 (float): The exponential decay rate for the first moment estimates in optimizer.
-        device (str): Device on which to train the model (e.g., 'cpu', 'cuda', 'mps').
-        is_display (bool): Flag to display progress during training.
-        history (dict): A dictionary to record training and testing loss over epochs.
+    | Parameter         | Type      | Description                                                                                                                                               |
+    |-------------------|-----------|-----------------------------------------------------------------------------------------------------------------------------------------------------------|
+    | epochs            | int       | The number of training epochs. Default is 10.                                                                                                             |
+    | lr                | float     | The learning rate for the optimizer. Default is 1e-4.                                                                                                     |
+    | loss              | str       | The loss function to use. Options include 'dice', 'dice_bce', 'IoU', 'focal', and 'combo'. Defaults to binary cross-entropy if not specified.            |
+    | is_attentionUNet  | bool      | If True, uses the AttentionUNet model, otherwise uses U-Net. Default is False.                                                                            |
+    | is_l1             | bool      | If True, applies L1 regularization. Default is False.                                                                                                     |
+    | is_l2             | bool      | If True, applies L2 regularization. Default is False.                                                                                                     |
+    | is_weight_clip    | bool      | If True, applies weight clipping to the model's parameters. Default is False.                                                                             |
+    | alpha             | float     | The alpha parameter for Focal loss and Combo loss. Default is 0.25.                                                                                       |
+    | gamma             | float     | The gamma parameter for Focal loss and Combo loss. Default is 2.                                                                                          |
+    | smooth            | float     | The smoothing parameter for Dice and Jaccard loss. Default is 0.01.                                                                                       |
+    | beta1             | float     | The exponential decay rate for the first moment estimates in Adam optimizer. Default is 0.5.                                                              |
+    | beta2             | float     | The exponential decay rate for the second moment estimates in Adam optimizer. Default is 0.999.                                                           |
+    | min_clip          | float     | The minimum value for weight clipping. Default is -0.01.                                                                                                   |
+    | max_clip          | float     | The maximum value for weight clipping. Default is 0.01.                                                                                                    |
+    | device            | str       | The device to run the model on (e.g., 'cpu', 'cuda', 'mps'). Default is 'mps'.                                                                             |
+    | display           | bool      | If True, displays training progress. Default is True.                                                                                                      |
 
-    Methods:
-        __setup__(): Sets up the model, loss function, optimizer, and data loaders.
-        select_loss_function(): Selects the loss function based on the 'loss' attribute.
-        l1_loss(model, lambda_value=0.01): Calculates the L1 loss for regularization.
-        l2_loss(model, lambda_value=0.01): Calculates the L2 loss for regularization.
-        update_training_loss(**kwargs): Performs a single training step.
-        update_testing_loss(**kwargs): Evaluates the model on the test set.
-        saved_checkpoints(**kwargs): Saves model checkpoints during training.
-        show_progress(**kwargs): Prints or logs the training progress.
-        train(): Main method to run the training loop.
-        plot_loss_curves(): Static method to plot the training and testing loss curves.
+    ### Notes:
+    - The device is automatically initialized based on the given `device` string.
+    - Weight clipping is applied if `is_weight_clip` is True, with limits set by `min_clip` and `max_clip`.
 
-    Examples:
-        >>> trainer = Trainer(epochs=10, lr=0.001, loss='dice', device='cuda')
-        >>> trainer.train()
+    ### Examples:
+    ```python
+    trainer = Trainer(
+        epochs=20,
+        lr=0.001,
+        loss='dice',
+        is_attentionUNet=True,
+        device='cuda'
+    )
+    trainer.train()
+    ```
+
+    This example initializes a Trainer object for training an Attention U-Net model with the Dice loss function on a CUDA device.
     """
 
     def __init__(
         self,
         epochs=10,
-        lr=0.0002,
+        lr=1e-4,
         loss=None,
+        is_attentionUNet=False,
+        is_l1=False,
+        is_l2=False,
+        is_weight_clip=False,
         alpha=0.25,
         gamma=2,
-        smooth_value=0.01,
+        smooth=0.01,
         beta1=0.5,
+        beta2=0.999,
+        min_clip=-0.01,
+        max_clip=0.01,
         device="mps",
         display=True,
     ):
@@ -69,78 +83,38 @@ class Trainer:
         self.lr = lr
         self.loss = loss
         self.alpha = alpha
+        self.is_attentionUNet = is_attentionUNet
+        self.is_l1 = is_l1
+        self.is_l2 = is_l2
+        self.is_weight_clip = is_weight_clip
         self.gamma = gamma
-        self.smooth_value = smooth_value
+        self.smooth = smooth
         self.beta1 = beta1
-        self.beta2 = 0.999
-        self.device = device
+        self.beta2 = beta2
+        self.min_clip = min_clip
+        self.max_clip = max_clip
+        self.device = device_init(device=device)
         self.is_display = display
         self.history = {"train_loss": list(), "test_loss": list()}
 
-    def __setup__(self):
-        self.device = define_device(device=self.device)
-        self.model = UNet().to(self.device)
-        self.model.apply(weight_init)
-        self.loss = self.select_loss_function()
-        self.optimizer = torch.optim.SGD(self.model.parameters(), lr=self.lr)
+        self.min_clip = self.min_clip
+        self.max_clip = self.max_clip
 
-        if os.path.exists(PROCESSED_PATH):
-            self.train_dataloader = load(
-                filename=os.path.join(PROCESSED_PATH, "train_dataloader.pkl")
-            )
-            self.test_dataloader = load(
-                filename=os.path.join(PROCESSED_PATH, "test_dataloader.pkl")
-            )
-        else:
-            raise ValueError(
-                "Processed data not found. Please run the preprocessing notebook.".capitalize()
-            )
+        self.setups = helpers(
+            is_attentionUNet=self.is_attentionUNet,
+            device=self.device,
+            lr=self.lr,
+            loss=self.loss,
+            smooth=self.smooth,
+            alpha=self.alpha,
+            gamma=self.gamma,
+        )
 
-    def select_loss_function(self):
-        """
-        Selects and returns the appropriate loss function based on the specified loss type during the initialization of the class. This method enables flexible switching between different loss functions for training, depending on the model's requirements and the specific task at hand.
-
-        The method supports various loss types, including 'dice', 'IoU' (Intersection over Union), 'focal', 'dice_bce' (a combination of Dice and Binary Cross-Entropy Loss), 'jaccard', and falls back to standard Binary Cross-Entropy Loss if none of these are specified.
-
-        Returns:
-            nn.Module: The selected loss function as a PyTorch module. The specific loss function returned depends on the 'loss' attribute defined during the initialization of the class.
-
-        Supported loss types and their corresponding returned loss functions:
-        - 'dice': Returns an instance of the `DiceLoss` class.
-        - 'IoU': Returns an instance of the `IoU` class.
-        - 'focal': Returns an instance of the `FocalLoss` class, with `alpha` and `gamma` parameters.
-        - 'dice_bce': Returns an instance of the `DiceBCE` class.
-        - 'jaccard': Returns an instance of the `JaccardLoss` class.
-        - Otherwise: Returns an instance of the `nn.BCELoss` class with `reduction` set to "mean".
-
-        Examples:
-        ```python
-        # Assuming 'self.loss' is set to "dice" and 'self.smooth_value' is defined
-        loss_function = self.select_loss_function()
-        # `loss_function` is now an instance of `DiceLoss`
-
-        # If 'self.loss' is set to "focal", with 'self.alpha' and 'self.gamma' defined
-        loss_function = self.select_loss_function()
-        # `loss_function` is now an instance of `FocalLoss`
-        ```
-        """
-        if self.loss == "dice":
-            return DiceLoss(smooth=self.smooth_value)
-        elif self.loss == "IoU":
-            return IoU(smooth=self.smooth_value)
-        elif self.loss == "focal":
-            return FocalLoss(alpha=self.alpha, gamma=self.gamma)
-        elif self.loss == "dice_bce":
-            return DiceBCE(smooth=self.smooth_value)
-        elif self.loss == "jaccard":
-            return JaccardLoss(smooth=self.smooth_value)
-        elif self.loss == "combo":
-            return ComboLoss(
-                alpha=self.alpha, gamma=self.gamma, smooth=self.smooth_value
-            )
-        else:
-            print(self.loss)
-            return nn.BCELoss(reduction="mean")
+        self.optimizer = self.setups["optimizer"]
+        self.model = self.setups["model"]
+        self.loss = self.setups["loss"]
+        self.train_dataloader = self.setups["train_dataloader"]
+        self.test_dataloader = self.setups["test_dataloader"]
 
     def l1_loss(self, model, lambda_value=0.01):
         """
@@ -186,9 +160,26 @@ class Trainer:
         self.optimizer.zero_grad()
 
         train_predicted_masks = self.model(kwargs["images"])
-        train_predicted_loss = self.loss(train_predicted_masks, kwargs["masks"])
+
+        if self.is_l1:
+            train_predicted_loss = self.loss(
+                train_predicted_masks, kwargs["masks"]
+            ) + self.l1_loss(self.model)
+
+        elif self.is_l2:
+            train_predicted_loss = self.loss(
+                train_predicted_masks, kwargs["masks"]
+            ) + self.l2_loss(self.model)
+
+        else:
+            train_predicted_loss = self.loss(train_predicted_masks, kwargs["masks"])
+
+        if self.is_weight_clip:
+            for params in self.model.parameters():
+                params.data.clamp_(-self.min_clip, self.max_clip)
 
         train_predicted_loss.backward(retain_graph=True)
+
         self.optimizer.step()
 
         return train_predicted_loss.item()
@@ -250,13 +241,8 @@ class Trainer:
                 )
             )
         elif self.is_display == False:
-            logging.info(
-                "Epochs: [{}/{}] - train_loss: [{:.5f}] - test_loss: [{:.5f}]".format(
-                    kwargs["epoch"],
-                    kwargs["epochs"],
-                    kwargs["train_loss"],
-                    kwargs["test_loss"],
-                )
+            print(
+                "Epochs - {}/{} is completed".format(kwargs["epoch"], kwargs["epochs"])
             )
 
     def train(self):
@@ -390,11 +376,14 @@ if __name__ == "__main__":
         "--loss", type=str, default="dice", help="Loss function".capitalize()
     )
     parser.add_argument(
+        "--attentionUNet", type=str, default=False, help="Attention UNet".capitalize()
+    )
+    parser.add_argument(
         "--display", type=bool, default=True, help="Display progress".capitalize()
     )
     parser.add_argument("--device", type=str, default="mps", help="Device".capitalize())
     parser.add_argument(
-        "--smooth_value", type=float, default=0.01, help="Smooth value".capitalize()
+        "--smooth", type=float, default=0.01, help="Smooth value".capitalize()
     )
     parser.add_argument(
         "--alpha", type=float, default=0.5, help="Alpha value".capitalize()
@@ -411,6 +400,7 @@ if __name__ == "__main__":
             args.epochs
             and args.lr
             and args.loss
+            and args.attentionUNet
             and args.display
             and args.device
             and args.smooth_value
@@ -421,12 +411,14 @@ if __name__ == "__main__":
                 epochs=args.epochs,
                 lr=args.lr,
                 loss=args.loss,
+                is_attentionUNet=args.attentionUNet,
                 alpha=args.alpha,
                 gamma=args.gamma,
                 display=args.display,
                 device=args.device,
-                smooth_value=args.smooth_value,
+                smooth=args.smooth,
             )
+
             trainer.train()
     else:
         raise Exception("Train flag is not set.".capitalize())
